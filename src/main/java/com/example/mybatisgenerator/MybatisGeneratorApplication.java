c155b8d9e50a3a13e64d2087be57652a56e047f7
orang <loveorange@163.com>
2019-02-24 13:09:20 +0800

    mybatis-genarator 1.3.7包含tk.mybatis、lombok

--- src/main/java/com/example/mybatisgenerator/MybatisGeneratorApplication.java
+++ src/main/java/com/example/mybatisgenerator/MybatisGeneratorApplication.java
@@ -1,10 +1,12 @@
 package com.example.mybatisgenerator;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@MapperScan("com.example.mybatisgenerator.mapper")
 public class MybatisGeneratorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MybatisGeneratorApplication.class, args);
